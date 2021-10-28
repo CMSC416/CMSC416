09f305b02284797121595d024da300047d91e510
Abhinav Bhatele <bhatele@cs.umd.edu>
2021-10-28 00:09:50 -0400

    Assignment 3: Sequential earthquake modeling code
    * comment out numthreads print statement (#7)
    
    * add job submission script (#8) and Makefile (#9)

---
+++ assignment-3/submit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Request 8 cores on a single node for 5 minutes
+#SBATCH --cpus-per-task=8
+#SBATCH -t 00:05:00
+#SBATCH --exclusive
+#SBATCH --mem-bind=local
+
+
+# This is to suppress the warning about not finding a GPU resource
+export OMPI_MCA_mpi_cuda_support=0
+
+# Env variable to reduce performance variability
+export OMP_PROCESSOR_BIND=true
+
+# Set the number of OpenMP threads to 8
+export OMP_NUM_THREADS=8
+
+# Run the executable
+./quake-omp < quake.in > quake.out
