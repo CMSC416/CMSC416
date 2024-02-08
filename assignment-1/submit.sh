b3adb07ff21836456f8975019250d157520609ca
Abhinav Bhatele <bhatele@cs.umd.edu>
2024-02-07 23:29:15 -0500

    Assignment 1: submit script

---
+++ assignment-1/submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Allocate 16 cores on a single node for 5 minutes
+#SBATCH -N 1
+#SBATCH --ntasks=16
+#SBATCH -t 00:05:00
+#SBATCH --exclusive
+
+
+# This is to suppress the warning about not finding a GPU resource
+export OMPI_MCA_mpi_cuda_support=0
+
+# Load OpenMPI
+module load openmpi/gcc
+
+# Run the executable
+mpirun -np 16 ./life &> myfile.out
