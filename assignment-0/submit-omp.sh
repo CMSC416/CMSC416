2fe7e927f4465da0f6a414402f7cc79de923966b
Abhinav Bhatele <bhatele@cs.umd.edu>
2024-09-11 09:57:07 -0400

    Assignments 0 and 1: update OpenMP submit scripts

--- assignment-0/submit-omp.sh
+++ assignment-0/submit-omp.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Allocate 16 cores on a single node for 5 minutes
+# Request 16 cores on a single node for 5 minutes
 #SBATCH -N 1
-#SBATCH --ntasks=16
+#SBATCH --cpus-per-task=16
 #SBATCH -t 00:05:00
 #SBATCH -A cmsc416-class
+
 
 # This is to suppress the warning about not finding a GPU resource
 export OMPI_MCA_mpi_cuda_support=0
 
-# Set the value of OMP_NUM_THREADS
+# Set the number of OpenMP threads to 16
 export OMP_NUM_THREADS=16
+
 
 # Run the OpenMP executable
 ./cpi-omp &> my-openmp.out
