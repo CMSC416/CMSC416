87eb30f4d6dc99f44d4f5e4a0ac1b342b90c63d6
Abhinav Bhatele <bhatele@cs.umd.edu>
2024-09-11 09:57:07 -0400

    Assignments 0 and 1: update OpenMP submit scripts

--- assignment-1/submit.sh
+++ assignment-1/submit.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Request 8 cores on a single node for 5 minutes
+#SBATCH -N 1
 #SBATCH --cpus-per-task=8
 #SBATCH -t 00:05:00
-#SBATCH --exclusive
+#SBATCH -A cmsc416-class
 #SBATCH --mem-bind=local
 
 
@@ -16,5 +17,6 @@ export OMP_PROCESSOR_BIND=true
 # Set the number of OpenMP threads to 8
 export OMP_NUM_THREADS=8
 
+
 # Run the executable
-./problem1 > problem1.out
+./problem1 &> problem1.out
