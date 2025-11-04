4c39c972009ae3c1436392623b96e0e4d959d2cd
Abhinav Bhatele <bhatele@cs.umd.edu>
2025-11-03 23:00:42 -0500

    rename

--- assignment-4/jacobi-2d/submit.sh
+++ assignment-4/jacobi-2d/submit.sh
@@ -3,28 +3,18 @@
 #SBATCH -p gpu
 #SBATCH --gres=gpu:a100_1g.5gb
 #SBATCH -A cmsc416-class
-#SBATCH -o cuda-jacobi-2d-%A.out
-#SBATCH -J cuda-jacobi-2d
+#SBATCH -o cuda-2d-stencil-%A.out
+#SBATCH -J cuda-2d-stencil
 #SBATCH -t 00:00:15
 
 # load cuda libraries
 module load cuda
 
-num_sms=80
+# first problem
+./jacobi-2d 32x32-input.csv 32x32-output.csv 100 32 32 1 1
 
-function run_jacobi_2d {
-  x=$1
-  y=$2
-  iterations=$3
-  input=${x}x${y}-input.csv
-  output=${x}x${y}-output.csv
-  grid_dims=$((32 * num_sms))
-  echo "Running jacobi-2d with input: ${input}, output: ${output}, iterations: ${iterations}"
-  ./jacobi-2d ${input} ${output} ${iterations} ${x} ${y} ${grid_dims} ${grid_dims}
-}
+# second problem
+./jacobi-2d 64x128-input.csv 64x128-output.csv 100 64 128 2 4
 
-INPUTS="32x32-input.csv 64x128-input.csv 128x128-input.csv"
-
-run_jacobi_2d 32 32 100
-run_jacobi_2d 64 128 100
-run_jacobi_2d 128 256 100
+# third problem
+./jacobi-2d 128x128-input.csv 128x128-output.csv 100 128 128 4 4
