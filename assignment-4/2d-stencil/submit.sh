c099cbc2b8b304b73eabd527257509228e1e137b
Abhinav Bhatele <bhatele@cs.umd.edu>
2025-11-03 23:17:48 -0500

    move output file to end

--- assignment-4/2d-stencil/submit.sh
+++ assignment-4/2d-stencil/submit.sh
@@ -11,10 +11,10 @@
 module load cuda
 
 # first problem
-./jacobi-2d 32x32-input.csv 32x32-output.csv 100 32 32 1 1
+./jacobi-2d 32x32-input.csv 100 32 32 1 1 32x32-output.csv
 
 # second problem
-./jacobi-2d 64x128-input.csv 64x128-output.csv 100 64 128 2 4
+./jacobi-2d 64x128-input.csv 100 64 128 2 4 64x128-output.csv
 
 # third problem
-./jacobi-2d 128x128-input.csv 128x128-output.csv 100 128 128 4 4
+./jacobi-2d 128x128-input.csv 100 128 128 4 4 128x128-output.csv
